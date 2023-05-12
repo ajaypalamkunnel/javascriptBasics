064fd410d0ee22d008cc509cd188ffed9c63e22b
Ajay ps <ajaypalamkunnel45@gmail.com>
2023-05-12 07:57:33 +0530

    operators completed

---
+++ script.js
@@ -0,0 +1,26 @@
+//operators
+//Arithematic operators
+
+var a = 10;
+var b = 5;
+
+document.write(a+b)
+document.write("<br>")
+document.write(a*b)
+document.write("<br>")
+document.write(a-b)
+document.write("<br>")
+document.write(a/b)
+document.write("<br>")
+document.write(a%b)
+document.write("<br>")
+a++
+document.write(a)
+
+document.write("<br>")
+
+//exponentional operators
+
+document.write(10 ** 2)//square
+document.write("<br>")
+document.write(10 ** 3)//cube
