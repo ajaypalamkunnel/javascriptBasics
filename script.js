3eb3e6c8b85fdd3d9e00caa5c7608f447c7e569c
Ajay ps <ajaypalamkunnel45@gmail.com>
2023-05-12 07:24:06 +0530

    variables completed next operators

---
+++ script.js
@@ -0,0 +1,40 @@
+var person = "John"
+var person2 = "smith"
+
+
+document.write(person)
+document.write("<br>")
+document.write(typeof person)
+
+
+//number
+
+var num1 = 10;
+var num2 = 20.34;
+
+document.write("<br>")
+document.write(num1)
+document.write("<br>")
+document.write(typeof num1)
+
+//Boolean
+
+var js = true;
+var html = false;
+
+//undefined
+
+var test1;
+
+
+
+document.write("<br>")
+document.write(test1)
+document.write("<br>")
+document.write(typeof test1)
+
+
+//null
+ var test2=null;
+
+
