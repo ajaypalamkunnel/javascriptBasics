598a6f9c8d14c17b3a7a42aaac9666490b78a73c
Ajay ps <ajaypalamkunnel45@gmail.com>
2023-05-12 15:48:10 +0530

    control statement switch completed

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-var person = "John"
-var person2 = "smith"
+var person = "John";
+var person2 = "smith";
 
-
-document.write(person)
-document.write("<br>")
-document.write(typeof person)
-
+document.write(person);
+document.write("<br>");
+document.write(typeof person);
 
 //number
 
 var num1 = 10;
 var num2 = 20.34;
 
-document.write("<br>")
-document.write(num1)
-document.write("<br>")
-document.write(typeof num1)
+document.write("<br>");
+document.write(num1);
+document.write("<br>");
+document.write(typeof num1);
 
 //Boolean
 
@@ -26,29 +24,40 @@ var html = false;
 
 var test1;
 
-
-
-document.write("<br>")
-document.write(test1)
-document.write("<br>")
-document.write(typeof test1)
-
+document.write("<br>");
+document.write(test1);
+document.write("<br>");
+document.write(typeof test1);
 
 //null
- var test2=null;
-
+var test2 = null;
 
 //controll statemnts
 
-var k = 10
+var k = 10;
 
-if(k>0)
-{
-    document.write("<br>"+"positive number")
-}else if(k<0)
-{
-    document.write("<br>"+"Negative number")
-}else{
-    document.write("<br>"+"Zero")
+if (k > 0) {
+  document.write("<br>" + "positive number");
+} else if (k < 0) {
+  document.write("<br>" + "Negative number");
+} else {
+  document.write("<br>" + "Zero");
 }
 
+document.write("<br>" + "Switch statement"+"<br>");
+
+var a = "C";
+
+switch (a) {
+  case "A":
+    document.write("Apple");
+    break;
+  case "B":
+    document.write("Bat");
+    break;
+  case "C":
+    document.write("cat");
+    break;
+  default:
+    document.write("Invalid entry");
+}
